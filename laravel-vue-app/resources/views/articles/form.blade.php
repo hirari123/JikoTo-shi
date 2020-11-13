26b00885f2c84ea20202c1a1ee1cbfc4540a587b
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2020-11-13 21:14:37 +0900

    articles_6blade

---
+++ laravel-vue-app/resources/views/articles/form.blade.php
@@ -0,0 +1,12 @@
+<!-- 記事タイトル・本文 -->
+
+@csrf
+<div class="md-form">
+  <label>タイトル</label>
+  <!-- 既存の記事内容を表示させる -->
+  <input type="text" name="title" class="form-control" required value="{{$article->title ?? old('title')}}">
+</div>
+<div class="form-group">
+  <label></label>
+  <textarea name="content" required class="form-control" rows="16" placeholder="本文">{{$article->content ?? old('content')}}</textarea>
+</div>
