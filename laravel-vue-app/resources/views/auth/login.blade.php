bf82a9d3dbf6152c163f715a235ce5b1f5482434
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2020-11-13 21:46:03 +0900

    views_auth

--- laravel-vue-app/resources/views/auth/login.blade.php
+++ laravel-vue-app/resources/views/auth/login.blade.php
@@ -32,6 +32,11 @@
                 <!-- 次回から自動でログインする -->
                 <input type="hidden" name="remember" id="remember" value="on">
 
+                <!-- ログイン画面からパスワード再設定メール要求画面へ遷移可能にする -->
+                <div class="text-left">
+                  <a href="{{route('password.request')}}" class="card-text">パスワードをお忘れの方</a>
+                </div>
+
                 <button class="btn btn-block aqua-gradient mt-2 mb-2" type="submit">ログイン</button>
 
               </form>
