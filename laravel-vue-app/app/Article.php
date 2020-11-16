8be5fd24d5500b7652858a36cf6a9dbfe436d2ee
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2020-11-16 18:20:30 +0900

    記事モデルの変更

--- laravel-vue-app/app/Article.php
+++ laravel-vue-app/app/Article.php
@@ -38,5 +38,10 @@ class Article extends Model
         return $this->likes->count();
     }
 
+    // 記事モデルからタグモデルへのリレーションの作成
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Tag')->withTimestamps();
+    }
 
 }
---
+++ laravel-vue-app/app/Tag.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+// タグモデル
+class Tag extends Model
+{
+
+    
+}
